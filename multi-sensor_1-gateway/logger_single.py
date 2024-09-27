e0f3a71da9f9971181c83b59105634eb5912f209
StavroGentile <stavro.gentile@outlook.com>
2024-09-27 19:22:28 -0400

    Fixed bugs

--- multi-sensor_1-gateway/logger_single.py
+++ multi-sensor_1-gateway/logger_single.py
@@ -100,14 +100,14 @@ class NanoIMUBLEClient:
         print('Looking for Peripheral Device...')
         devices = None
         devices = await BleakScanner.discover()
-        if devices is not None:
-            for d in devices:
-                local_name = d.name or 'Unknown'
-                if local_name == TARGET_TAG_NAME:
-                    self._found = True
-                    self._device = d
-                    print(f'Found Peripheral Device {self._device.address}. Local Name: {local_name}')
-                    return
+        for d in devices:
+            local_name = d.name or 'Unknown'
+            if local_name == TARGET_TAG_NAME and (d.rssi > -60):
+                print(f"RSSI: {d.rssi}")
+                self._found = True
+                self._device = d
+                print(f'Found Peripheral Device {self._device.address}. Local Name: {local_name}')
+                return
 
         print("Peripheral device not found. Retry...")
         await asyncio.sleep(1)
